443dac8d48b47ae3339270f287e53e9bda4f9664
roufrubel <rubel.ma2012@gmail.com>
2024-07-06 16:43:58 +0600

    delete method apllied

--- index.js
+++ index.js
@@ -57,6 +57,13 @@ async function run() {
       console.log(result)
     })
 
+    app.delete('/craft/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };     
+      const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
